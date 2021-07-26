1db31166e20819587ad93c2c5786913faa2d06e2
Daniel Shen <danshen@berkeley.edu>
2021-07-26 22:37:35 +0800

    Fix mobile keyboard popup affecting html elements

--- draw/static/draw/script.js
+++ draw/static/draw/script.js
@@ -1,6 +1,7 @@
+window.onload = (event) => {
   // simulate userDB; initialize users and randomly assign each user a name
-  var uid = Math.floor(Math.random() * 5);
-  var usernames = ["Emily", "John", "Ben", "Nia", "Stephanie", "Aisha"]
+  var uid = Math.floor(Math.random() * 10);
+  var usernames = ["Emily", "John", "Ben", "Nia", "Stephanie", "Aisha", "Alan", "Chris", "Nisha", "Akira"]
   var listDataBase = {}
   var validater = {}
 
@@ -8,13 +9,29 @@
   var socket = new WebSocket('ws://' + window.location.host + '/ws/draw');
 
 
-  // button interactions
-  $("#Name").focus(function() {
+  // fix viewport to address keyboard resizing html issue
+  // source: stackoverflow @Tyler Merle
+  setTimeout(function() {
+    let viewheight = $(window).height();
+    let viewwidth = $(window).width();
+    let viewport = document.querySelector("meta[name=viewport]");
+    viewport.setAttribute("content", "height=" + viewheight + ", width=" + viewwidth + ", initial-scale=1.0");
+  }, 300);
+
+  // set initial date
+  let today = new Date();
+  let month = today.getMonth() + 1;
+  if (month < 10) {
+    month = "0" + month;
+  }
+  $('#Date, #Date2').val(today.getFullYear() + "-" + month + "-" + today.getDate());
+
+  $("#Name, #Name2").focus(function() {
     $(this).val("");
     $(this).css("color", "black")
   })
 
-  $("#Name").focusout(function() {
+  $("#Name, #Name2").focusout(function() {
     if ($(this).val() == "") {
       $(this).val("Enter task...")
       $(this).css("color", "rgb(104, 104, 104)")
@@ -22,43 +39,19 @@
   })
 
   $("#Name2").focus(function() {
-    $(this).val("");
-    $(this).css("color", "black")
+    $(".btnDiv").css("visibility", "hidden");
   })
 
   $("#Name2").focusout(function() {
-    if ($(this).val() == "") {
-      $(this).val("Enter task...")
-      $(this).css("color", "rgb(104, 104, 104)")
-    }
+    $(".btnDiv").css("visibility", "visible");
   })
 
   $("#Add").click(function() {
-    var name = $("#Name").val()
-    var date = $("#Date").val()
-    var task = $("#Task").val()
+    var name = $("#Name, #Name2").val()
+    var date = $("#Date, #Date2").val()
+    var task = $("#Task, #Task2").val()
 
-    if (name && date && ( validater[name] == undefined || (validater[name][0] !==date && validater[name][1] !==task))) {
-
-      var tDiv = $(('<div>')).addClass("taskBar");
-      tDiv.append(
-        $(('<div>')).html(`<label for="${name}" id="taskName">${name}</label><label for="${date}">Deadline: ${date}</label>`).addClass("taskItem"),
-        $(('<div>')).html(`<input type="checkbox" value="${name};${date};${task}">`).addClass("taskCheck")
-      )
-      tDiv.addClass("animate__animated animate__fadeIn animate__fast")
-      $('#taskDiv').append(tDiv);
-      validater[name] = [date, task]
-    } else {
-      alert("You got duplicated task OR enpty name/date")
-    }
-  })
-
-  $("#Add2").click(function() {
-    var name = $("#Name2").val()
-    var date = $("#Date2").val()
-    var task = $("#Task2").val()
-
-    if (name && date && ( validater[name] == undefined || (validater[name][0] !==date && validater[name][1] !==task))) {
+    if (name && date && (validater[name] == undefined || (validater[name][0] !== date && validater[name][1] !== task))) {
 
       var tDiv = $(('<div>')).addClass("taskBar");
       tDiv.append(
@@ -66,12 +59,13 @@
         $(('<div>')).html(`<input type="checkbox" value="${name};${date};${task}">`).addClass("taskCheck")
       )
       tDiv.addClass("animate__animated animate__fadeIn animate__fast")
-      $('#taskDiv2').append(tDiv);
+      $('#taskDiv, #taskDiv2').append(tDiv);
       validater[name] = [date, task]
     } else {
-      alert("You got duplicated task OR enpty name/date")
+      alert("You got duplicated task OR empty name/date")
     }
   })
+
 
   $("#Remove").click(function() {
     var checkboxes = $(":checkbox")
@@ -91,7 +85,6 @@
       if (item.checked) {
         var obj = {};
         obj.uid = uid;
-        console.log(item.value)
         obj.name = item.value.split(";")[0]
         obj.date = item.value.split(";")[1]
         obj.kind = '#' + item.value.split(";")[2] + 'Tasks';
@@ -106,7 +99,6 @@
 
   socket.onmessage = function(receivedMessage) {
     var received = JSON.parse(receivedMessage.data);
-    console.log(received)
     // bigScreen Data display
     uname = usernames[received.uid].toString();
     listDataBase[uname] = received.task;
@@ -115,16 +107,15 @@
     $('#designTasks').html("");
     $('#deployTasks').html("");
     $('#reviewTasks').html("");
+
     Object.keys(listDataBase).forEach(function(key) {
       listDataBase[key].forEach(task => {
         $(task.kind).append($(('<div>')).html(`${task.name}<br>Deadline:<br>${task.date}<br>Posted by:${usernames[task.uid]}`).addClass("bigScreenTasks animate__animated animate__fadeIn animate__fast"));
       })
     })
-    // received.task.forEach(task => {  
-    //   $('#testTasks').append($(('<div>')).html(`${task.name}<br>Deadline:<br>${task.date}<br>Posted by:${usernames[task.uid]}`).addClass("bigScreenTasks"));
-    // })
   }
 
   socket.onclose = function(e) {
     console.error('Chat socket closed unexpectedly');
   };
+};
